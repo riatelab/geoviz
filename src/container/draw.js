9455a96bbea0048ba39100500c4761b30351ecd9
neocarto <nicolas.lambert@cnrs.fr>
2024-05-14 13:52:22 +0200

    update doc

--- src/container/draw.js
+++ src/container/draw.js
@@ -2,6 +2,45 @@ import { create } from "./create";
 import { geoEquirectangular } from "d3-geo";
 const d3 = Object.assign({}, { geoEquirectangular });
 
+/**
+ * @function draw
+ * @description The `draw` function is inspired by the [`bertin`](https://observablehq.com/@neocartocnrs/hello-bertin-js?collection=@neocartocnrs/bertin) library. It allows you to draw the entire map using a single function. If you like the `bertin` lib, you'll like this function. As in `bertin`, all the necessary information is stored in a single JSON file, containing general parameters and a table describing the layers to be displayed and overlaid.
+ * @see {@link https://observablehq.com/@neocartocnrs/geoviz-draw}
+ *
+ * @property {object} [json] - a objection containing params et layers
+
+ *
+ * @example
+ * geoviz.draw({
+  params: {
+    zoomable: true
+    //projection: d3.geoNaturalEarth1()
+    //domain: world
+    //   margin: 100
+  },
+  layers: [
+    { type: "outline" },
+    { type: "graticule", stroke: "white", step: 30, strokeWidth: 2 },
+    //{ type: "tile" },
+    { type: "base", data: world },
+    {
+      type: "prop",
+      data: world,
+      symbol: "circle",
+      var: "gdp",
+      symbol: "square",
+      tip: true,
+      //leg_type: "extended",
+      leg_values_factor: 1 / 1000000000
+    },
+    { type: "base", data: aus },
+    { type: "base", data: aus_roads, tip: true },
+    { type: "text", data: aus_roads, text: "type" },
+    { type: "header", text: "Carte du monde" }
+  ]
+})
+ */
+
 export function draw(json) {
   let params = json?.params || {};
   // layer types
